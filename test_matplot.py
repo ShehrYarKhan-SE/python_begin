87f7a0e72aef7d639d9bf829fd22db819035492e
ShehrYar Khan <khanshehryarkhan21@gmail.com>
2025-07-05 16:00:08 +0500

    update

--- test_matplot.py
+++ test_matplot.py
@@ -1,2 +1,8 @@
-import matplotlib as mtplt
-print(mtplt.__version__)
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
